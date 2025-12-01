03c96cbd00599e6b22090e986d47ccdbf36bb335
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-12-01 10:31:53 +0530

    Create MISSING NO.java

---
+++ MISSING NO.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int missingno = nums.length;
+        for(int i=0;i<nums.length;i++){
+            missingno ^= i^nums[i];
+        }
+        return missingno;
+    }
+}
