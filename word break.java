d1fac54e323bc89574af268344a8946d18f6f5a9
kamlakant kumar <151719768+kamlakantkumar51@users.noreply.github.com>
2025-12-09 19:37:18 +0530

    Update word break.java

--- word break.java
+++ word break.java
@@ -20,3 +20,23 @@ class Solution {
         return false;
     }
 }
+//optimised code by dp
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        Set<String> dict = new HashSet<>(wordDict);
+        boolean dp[] = new boolean[n+1];
+        dp[0] = true;
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                if(dp[j] && dict.contains(s.substring(j,i))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+
+    }
+}
